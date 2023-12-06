152a63cdbc9ca63de407cefd191e9e8919e7855f
carldamey <carldamey@gmail.com>
2023-12-05 18:09:27 -0800

    stubs basic site components and imports to app.jsx

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css"
 import NavBar from "../../components/NavBar/NavBar"
+import About from "../../components/About/About"
+import Skills from "../../components/Skills/Skills"
+import Projects from "../../components/Projects/Projects"
+import Contact from "../../components/Contact/Contact"
 
 export default function App() {
 	return (
 		<main className="App">
 			<NavBar />
-			<h1>Carl Damey</h1>
+			<About />
+			<Skills />
+			<Projects />
+			<Contact />
 		</main>
 	)
 }
